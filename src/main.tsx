10188f04cc0d08307a69195037ebe072ba74d5a0
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-20 10:54:20 +0200

    Users array

--- src/main.tsx
+++ src/main.tsx
@@ -6,28 +6,48 @@ window.React = React;
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-const user = {
-  id: "123",
-  name: "Alice",
-  pet: { name: "Cat" },
-  color: "red",
-};
-
-const vdiv = React.createElement(
-  "div",
+const users = [
   {
-    id: user.id,
-    title: user.name,
-    className: "person-card",
-    style: {
-      color: user.color,
-    },
+    id: "123",
+    name: "Alice",
+    pet: { name: "Cat" },
+    color: "red",
   },
-  React.createElement("p", {}, `${user.name} as a ${user.pet.name}`),
-  React.createElement("input", { key: "pamietaj mnie" })
-);
+  {
+    id: "234",
+    name: "Bob",
+    pet: { name: "Dog" },
+    color: "green",
+  },
+  {
+    id: "345",
+    name: "Kate",
+    pet: { name: "Parrot" },
+    color: "blue",
+  },
+];
+const user = users[0];
 
-root.render(vdiv);
+const PersonCard = (props) =>
+  React.createElement(
+    "div",
+    {
+      id: props.user.id,
+      title: props.user.name,
+      className: "person-card",
+      style: {
+        color: props.user.color,
+      },
+    },
+    React.createElement(
+      "p",
+      {},
+      `${props.user.name} as a ${props.user.pet.name}`
+    )
+    // React.createElement("input", { key: "pamietaj mnie" })
+  );
+
+root.render(PersonCard(users[1]));
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   <React.StrictMode>
