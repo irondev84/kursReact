5ef8283c9e9a06963958cb7776a4940fd5940ae0
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-20 14:15:37 +0200

    Extracting function type from usage

--- src/playlists/components/PlaylistEditor.tsx
+++ src/playlists/components/PlaylistEditor.tsx
@@ -10,29 +10,34 @@ const PlaylistEditor = (props: Props) => {
     description: "Best playlist",
   };
 
-  const inputChangeHandler = (event: /* ??? */) => {
-    console.log(event);
-    
-  }
+  const eventHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {};
+    const placki = (e: React.MouseEvent<HTMLInputElement, MouseEvent>): void => { };
+  //   const inputChangeHandler = (event: 1234) => {
+  //   const changeHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {};
+  //   const changeHandler: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<T>) => {};
+  //   const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 
   return (
     <div>
       <div className="flex flex-col gap-5">
         <div className="flex flex-col">
           <strong>Name</strong>
-          <input type="text" onClick={inputChangeHandler} />
+          <input type="text" onChange={eventHandler}  onClick={placki} />
         </div>
-        
+
         <div className="flex">
           <input type="checkbox" defaultChecked={playlist.public} />
           <strong>Public</strong>
         </div>
-        
+
         <div className="flex flex-col">
           <strong>Description</strong>
-          <textarea value={playlist.description} readOnly={true} disabled={true}></textarea>
+          <textarea
+            value={playlist.description}
+            readOnly={true}
+            disabled={true}
+          ></textarea>
         </div>
-
       </div>
     </div>
   );
