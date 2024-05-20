435b24e1bfa56dd69cb23932b0781ef55064d74d
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-20 10:47:11 +0200

    React Virtual DOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+window.React = React;
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+const user = {
+  id: "123",
+  name: "Alice",
+  pet: { name: "Cat" },
+  color: "red",
+};
+
+const vdiv = React.createElement(
+  "div",
+  {
+    id: user.id,
+    title: user.name,
+    className: "person-card",
+    style: {
+      color: user.color,
+    },
+  },
+  React.createElement("p", {}, `${user.name} as a ${user.pet.name}`),
+  React.createElement("input", { key: "pamietaj mnie" })
+);
+
+root.render(vdiv);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
