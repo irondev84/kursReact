3265ec25791d459eb3fc1e436251d6ea89aae2b6
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-21 16:14:43 +0200

    Search Mockup

--- src/music/containers/MusicSearchView.tsx
+++ src/music/containers/MusicSearchView.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import PageLayout from "../../shared/components/PageLayout";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { Card } from "primereact/card";
 
 type Props = {};
 
@@ -7,8 +11,33 @@ const MusicSearchView = (props: Props) => {
   return (
     <PageLayout title="Music Search">
       <div className="grid columns-1 gap-5">
-        <div>Search Form</div>
-        <div>Search Results</div>
+        <div>
+          <div className="p-inputgroup flex-1">
+            <InputText placeholder="Keyword" />
+            <Button>Search</Button>
+          </div>
+        </div>
+        <div>
+          <div className="grid grid-cols-4 gap-5">
+            {[1, 2, 3, 4, 5].map((album) => (
+              <Card
+                title={`Album ${album}`}
+                subTitle="Card subtitle"
+                footer={(elem) => (
+                  <div>
+                    <Button>Details</Button>
+                  </div>
+                )}
+                header={
+                  <img src="https://primefaces.org/cdn/primereact/images/usercard.png" />
+                }
+                className="md:w-25rem"
+              >
+                {/* children */}
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </PageLayout>
   );
