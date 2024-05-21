f9f02f95551a2d3aae67ce6d1a6dbd67bded0154
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-21 16:48:10 +0200

    Ky / Axios /etc.

--- src/shared/musicAPI.tsx
+++ src/shared/musicAPI.tsx
@@ -1,13 +1,26 @@
-import { mockAlbums } from "./fixtures/mockAlbums";
+import ky from "ky";
 import { AlbumResponse } from "./types/Album";
 
-export const searchAlbums = async (query: string): Promise<AlbumResponse[]> => {
-  return fetch(
-    `https://api.spotify.com/v1/search?${new URLSearchParams({
-      q: "batman",
-      type: "album",
-    })}`
-  ).then((res) =>
-    res.ok ? res.json() : res.json().then((e) => Promise.reject(e))
-  );
+// export const searchAlbums = async (query: string): Promise<AlbumResponse[]> => {
+//   const res = await fetch(
+//     `https://api.spotify.com/v1/search?${new URLSearchParams({
+//       q: "batman",
+//       type: "album",
+//     })}`
+//   );
+
+//   return await (res.ok
+//     ? res.json()
+//     : res.json().then((e) => Promise.reject(e)));
+// };
+
+export const searchAlbums = async (query: string) => {
+  return ky
+    .get(`https://api.spotify.com/v1/search`, {
+      searchParams: {
+        q: query,
+        type: "album",
+      },
+    })
+    .json<AlbumResponse[]>();
 };
