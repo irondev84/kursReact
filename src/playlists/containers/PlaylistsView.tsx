2b309d304a88d77ff9b3e04f4b871a5eecf61037
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-22 09:34:37 +0200

    cleanup

--- src/playlists/containers/PlaylistsView.tsx
+++ src/playlists/containers/PlaylistsView.tsx
@@ -10,19 +10,8 @@ import { mockPlaylists } from "../../shared/fixtures/mockPlaylists";
 
 type Props = {};
 
-// React Hook "useState" cannot be called at the top level.
-// React Hooks must be called in a React function component or a custom React Hook function.eslintreact-hooks/rules-of-hooks
-// const [selectedId, setSelectedId] = useState<string | undefined>();
-
 const PlaylistsView = (props: Props) => {
   const [mode, setMode] = useState<"details" | "editor" | "creator">("details");
-
-  if (Math.random() > 0.5) {
-    // Warning: React has detected a change in the order of Hooks called by PlaylistsView. This will lead to bugs and errors if not fixed. 
-    // For more information, read the Rules of Hooks: https://reactjs.org/link/rules-of-hooks
-    // React Hook "useState" is called conditionally. React Hooks must be called in the exact same order in every component render.eslintreact-hooks/rules-of-hooks 
-    // const [selectedId, setSelectedId] = useState<string | undefined>();
-  }
 
   const [selectedId, setSelectedId] = useState<string | undefined>();
   const [selected, setSelected] = useState<Playlist | undefined>();
@@ -38,9 +27,6 @@ const PlaylistsView = (props: Props) => {
   const selectPlaylistById = (id: string) => setSelectedId(id);
 
   const createPlaylist = (draft: Playlist) => {
-    // React Hook "useState" is called in function "createPlaylist" that is neither a React function component nor a custom React Hook function.
-    // const [selectedId, setSelectedId] = useState<string | undefined>();
-
     draft.id = crypto.randomUUID();
     setPlaylists(addItem(draft));
     showDetails();
--- src/shared/hooks/useAutoFocus.tsx
+++ src/shared/hooks/useAutoFocus.tsx
@@ -1,33 +1,11 @@
 import { useEffect, useRef } from "react";
 
-
-
-/* 
-
-React Hook "useRef" is called in function "plackiAutoFocus" that is neither a React function component nor a custom React Hook function. 
-React component names must start with an uppercase letter. 
-React Hook names must start with the word "use".
-eslintreact-hooks/rules-of-hooks
-
-export function plackiAutoFocus() {
-  const ref = useRef<HTMLInputElement>(null);
-  // ...
-}
-*/
-
 /**
  * Use to attach autoFocus
  * @returns React.refObject
  */
 export function useAutoFocus() {
   const ref = useRef<HTMLInputElement>(null);
-
-
-  if(Math.random() > 0.5){
-    // React Hook "useState" is called conditionally. React Hooks must be called in the exact same order in every component render.eslintreact-hooks/rules-of-hooks
-    // const [first, setfirst] = useState(second)
-  }
-
   useEffect(() => ref.current?.focus(), []);
 
   return ref;
