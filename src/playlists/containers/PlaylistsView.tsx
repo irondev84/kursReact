8ad87dee5b507c44cb7f1137b93dbd9921426358
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-22 14:23:36 +0200

    useCallback + deps

--- src/playlists/containers/PlaylistsView.tsx
+++ src/playlists/containers/PlaylistsView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import PlaylistEditor from "../components/PlaylistEditor";
 import PlaylistDetails from "../components/PlaylistDetails";
 import PlaylistList from "../components/PlaylistList";
@@ -17,21 +17,27 @@ const PlaylistsView = (props: Props) => {
   const [selected, setSelected] = useState<Playlist | undefined>();
   const [playlists, setPlaylists] = useState<Playlist[]>(mockPlaylists);
 
-  const showDetails = () => setMode("details");
+  const showDetails = useCallback(() => setMode("details"), []);
   const showEditor = () => setMode("editor");
 
   useEffect(() => {
     setSelected(playlists.find((p) => p.id == selectedId));
   }, [selectedId, playlists]);
 
-  const selectPlaylistById = (id: string) => setSelectedId(id);
+  // const [selectPlaylistById] = useState(() => (id: string) => setSelectedId(id));
+  const selectPlaylistById = useCallback((id: string) => setSelectedId(id), []);
 
-  const createPlaylist = (draft: Playlist) => {
-    draft.id = crypto.randomUUID();
-    setPlaylists(addItem(draft));
-    showDetails();
-    setSelectedId(draft.id);
-  };
+  const createPlaylist = useCallback(
+    (draft: Playlist) => {
+      draft.id = crypto.randomUUID();
+      // setPlaylists(addItem(draft));
+      // setPlaylists([...playlists, draft]);
+      setPlaylists((prevPlaylist) => [...prevPlaylist, draft]);
+      showDetails();
+      selectPlaylistById(draft.id);
+    },
+    [showDetails, selectPlaylistById /* , playlists */]
+  );
 
   const updatePlaylist = (draft: Playlist) => {
     setPlaylists(replaceItemById(draft));
