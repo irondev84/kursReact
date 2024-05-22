3397ba2e96b7b126ea5dfb09169bb5b3f352e86a
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-22 14:52:34 +0200

    State, Reducer, actionCreator

--- src/playlists/containers/PlaylistsView.tsx
+++ src/playlists/containers/PlaylistsView.tsx
@@ -1,26 +1,42 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo, useReducer } from "react";
 import PlaylistEditor from "../components/PlaylistEditor";
 import PlaylistDetails from "../components/PlaylistDetails";
 import PlaylistList from "../components/PlaylistList";
 import { Button } from "primereact/button";
 import PageLayout from "../../shared/components/PageLayout";
 import { usePlaylistViewState } from "./usePlaylistViewState";
+import {
+  createPlaylistAction,
+  initialState,
+  reducer,
+  selectPlaylistAction,
+  setModeAction,
+  updatePlaylistAction,
+} from "../../store/playlists.reducer";
+import { Playlist } from "../../shared/types/Playlist";
 
 type Props = {};
 
 const PlaylistsView = (props: Props) => {
-  const {
-    playlists,
-    selectedId,
-    selectPlaylistById,
-    setMode,
-    mode,
-    selected,
-    showEditor,
-    updatePlaylist,
-    showDetails,
-    createPlaylist,
-  } = usePlaylistViewState();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { mode, playlists, selectedId } = state;
+
+  const selected = useMemo(
+    () => playlists.find((p) => p.id === selectedId),
+    [playlists, selectedId]
+  );
+
+  const selectPlaylistById = (id: string) => dispatch(selectPlaylistAction(id));
+
+  const setMode = (id: "details" | "editor" | "creator") =>
+    dispatch(setModeAction(id));
+
+  const updatePlaylist = (draft: Playlist) =>
+    dispatch(updatePlaylistAction(draft));
+
+  const createPlaylist = (draft: Playlist) =>
+    dispatch(createPlaylistAction(draft));
 
   return (
     <>
@@ -42,19 +58,25 @@ const PlaylistsView = (props: Props) => {
 
           <div className="flex flex-col gap-5">
             {mode === "details" && (
-              <PlaylistDetails playlist={selected} onEdit={showEditor} />
+              <PlaylistDetails
+                playlist={selected}
+                onEdit={() => setMode("editor")}
+              />
             )}
 
             {mode !== "editor" || (
               <PlaylistEditor
                 playlist={selected}
                 onSave={updatePlaylist}
-                onCancel={showDetails}
+                onCancel={() => setMode("details")}
               />
             )}
 
             {mode !== "creator" || (
-              <PlaylistEditor onSave={createPlaylist} onCancel={showDetails} />
+              <PlaylistEditor
+                onSave={createPlaylist}
+                onCancel={() => setMode("details")}
+              />
             )}
 
             <div></div>
--- src/store/playlists.reducer.ts
+++ src/store/playlists.reducer.ts
@@ -1,4 +1,5 @@
 import { mockPlaylists } from "../shared/fixtures/mockPlaylists"
+import { addItem, replaceItemById } from "../shared/helpers/addItem"
 import { Playlist } from "../shared/types/Playlist"
 
 
@@ -9,27 +10,68 @@ interface PlaylistsState {
 }
 // selected: Playlist | undefined
 
-const state: PlaylistsState = {
+export const initialState: PlaylistsState = {
     mode: 'details',
     playlists: mockPlaylists
 }
 
-
-function reducer(state: PlaylistsState, action: ActionTypes): PlaylistsState {
+export function reducer(state: PlaylistsState, action: ActionTypes): PlaylistsState {
+    console.log(action.type, action.payload);
 
     switch (action.type) {
         case 'PLAYLISTS_SET_MODE': return {
             ...state,
             mode: action.payload.mode
         }
+        case 'PLAYLISTS_SELECT_BY_ID': return {
+            ...state,
+            selectedId: action.payload.id
+        }
+        case 'PLAYLISTS_UPDATE': {
+            const draft = action.payload.draft
+            return {
+                ...state,
+                playlists: replaceItemById(draft)(state.playlists),
+                mode: 'details',
+                selectedId: draft.id
+            }
+        }
+        case 'PLAYLISTS_CREATE': {
+            const draft = action.payload.draft
+            return {
+                ...state,
+                playlists: addItem(draft)(state.playlists),
+                mode: 'details',
+                selectedId: draft.id
+            }
+        }
     }
-
+    action satisfies never; // All actions are checked!
     return state
 }
 
-type ActionTypes = ReturnType<typeof setMode>
+type ActionTypes = ReturnType<
+    | typeof setModeAction
+    | typeof selectPlaylistAction
+    | typeof updatePlaylistAction
+    | typeof createPlaylistAction
+>
 
-
-const setMode = (mode: PlaylistsState['mode']) => ({
+export const setModeAction = (mode: PlaylistsState['mode']) => ({
     type: 'PLAYLISTS_SET_MODE', payload: { mode }
-}) as const // Don't widen types to string/number - use LITERAL type
+}) as const
+
+export const selectPlaylistAction = (id: Playlist['id']) => ({
+    type: 'PLAYLISTS_SELECT_BY_ID', payload: { id }
+}) as const
+
+export const updatePlaylistAction = (draft: Playlist) => ({
+    type: 'PLAYLISTS_UPDATE', payload: { draft }
+}) as const
+
+export const createPlaylistAction = (draft: Playlist) => {
+    draft.id = crypto.randomUUID();
+    return ({
+        type: 'PLAYLISTS_CREATE', payload: { draft }
+    }) as const
+}
