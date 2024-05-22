dab746c88a2560e07e958591ae36bd294a56a886
Mateusz Kulesza <ev45ive@gmail.com>
2024-05-22 09:31:44 +0200

    Rules of Hooks - static hooks order

--- src/playlists/containers/PlaylistsView.tsx
+++ src/playlists/containers/PlaylistsView.tsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from "react";
 import PlaylistEditor from "../components/PlaylistEditor";
 import PlaylistDetails from "../components/PlaylistDetails";
 import PlaylistList from "../components/PlaylistList";
-import { mockPlaylists } from "./mockPlaylists";
-import { Playlist } from "./Playlist";
+import { Playlist } from "../../shared/types/Playlist";
 import { Button } from "primereact/button";
-import { addItem, replaceItemById } from "./addItem";
+import { addItem, replaceItemById } from "../../shared/helpers/addItem";
 import PageLayout from "../../shared/components/PageLayout";
+import { mockPlaylists } from "../../shared/fixtures/mockPlaylists";
 
 type Props = {};
+
+// React Hook "useState" cannot be called at the top level.
+// React Hooks must be called in a React function component or a custom React Hook function.eslintreact-hooks/rules-of-hooks
+// const [selectedId, setSelectedId] = useState<string | undefined>();
 
 const PlaylistsView = (props: Props) => {
   const [mode, setMode] = useState<"details" | "editor" | "creator">("details");
 
+  if (Math.random() > 0.5) {
+    // Warning: React has detected a change in the order of Hooks called by PlaylistsView. This will lead to bugs and errors if not fixed. 
+    // For more information, read the Rules of Hooks: https://reactjs.org/link/rules-of-hooks
+    // React Hook "useState" is called conditionally. React Hooks must be called in the exact same order in every component render.eslintreact-hooks/rules-of-hooks 
+    // const [selectedId, setSelectedId] = useState<string | undefined>();
+  }
+
   const [selectedId, setSelectedId] = useState<string | undefined>();
   const [selected, setSelected] = useState<Playlist | undefined>();
-  const [playlists, setPlaylists] = useState(mockPlaylists);
+  const [playlists, setPlaylists] = useState<Playlist[]>(mockPlaylists);
 
   const showDetails = () => setMode("details");
   const showEditor = () => setMode("editor");
@@ -27,6 +38,9 @@ const PlaylistsView = (props: Props) => {
   const selectPlaylistById = (id: string) => setSelectedId(id);
 
   const createPlaylist = (draft: Playlist) => {
+    // React Hook "useState" is called in function "createPlaylist" that is neither a React function component nor a custom React Hook function.
+    // const [selectedId, setSelectedId] = useState<string | undefined>();
+
     draft.id = crypto.randomUUID();
     setPlaylists(addItem(draft));
     showDetails();
